c05776ae258a7ad92948210d01ae32de6ff3c532
ohsobada-ctrl <ohsobada@gmail.com>
2025-12-29 20:35:28 +0000

    Add GitHub Action for Android Build

---
+++ android/app/src/main/java/com/carmanager/ly/MainActivity.java
@@ -0,0 +1,5 @@
+package com.carmanager.ly;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.carmanager.ly',
+  appName: 'Car Manager',
+  webDir: 'dist'
+};
+
+export default config;
